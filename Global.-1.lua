60812eb9f5fb33a44275bdd17105492a20e0f629
Benjamin O'Neill <ben.w.oneill@gmail.com>
2025-02-28 12:30:59 -0800

    Alert dial update

--- Global.-1.lua
+++ Global.-1.lua
@@ -38,7 +38,7 @@ rulerScale = 12/18.330303
 
 ASSETS = {
     ruler_12in = {
-        object = {type = "Custom_Token", scale = {rulerScale, 1, rulerScale}},
+        object = {type = "Custom_Token", scale = {12/18.330303, 1, 12/18.330303}},
         custom = {image = "https://steamusercontent-a.akamaihd.net/ugc/2178114146521158142/F80ED8E2A7D73712BDCF91587EA249D1B2E5B164/", thickness = 0.1}
     },
     turning_tool = {
@@ -47,6 +47,12 @@ ASSETS = {
             mesh = "https://steamusercontent-a.akamaihd.net/ugc/53576717526034505/A88863F87EC4E8E6A3B66EC813FC3E459700BBFD/",
             diffuse = "https://steamusercontent-a.akamaihd.net/ugc/2325615279340409003/08D9A7248BAC7E73E539F58552FCF7B7F7B86ED5/",
             collider = "https://steamusercontent-a.akamaihd.net/ugc/53580341274765070/1C959D9EA91D01CE2228CBE23C78E55F68F8F48F/"
+        }
+    },
+    constellation = {
+        alert_dial = {
+            object = {type = "Custom_Token"},
+            custom = {}
         }
     }
 }
