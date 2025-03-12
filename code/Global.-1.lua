cc9a1347ac3877af010dcbb5a74d0a7c26ace75e
Benjamin O'Neill <ben.w.oneill@gmail.com>
2025-03-12 15:46:06 -0700

    Dice, tools, and ship board mesh

--- code/Global.-1.lua
+++ code/Global.-1.lua
@@ -222,15 +222,15 @@ ASSET_ROOT = "https://raw.githubusercontent.com/bwoneill/tts_star_trek_into_the_
 ASSETS = {
     ruler_12in = {
         object = {type = "Custom_Token", scale = {12/18.330303, 1, 12/18.330303}},
-        custom = {image = "https://steamusercontent-a.akamaihd.net/ugc/2178114146521158142/F80ED8E2A7D73712BDCF91587EA249D1B2E5B164/", thickness = 0.1},
+        custom = {image = ASSET_ROOT .. "tools/ruler/ruler_12.png", thickness = 0.1},
         tag = "ruler",
     },
     turning_tool = {
         object = {type = "Custom_Model"},
         custom = {
-            mesh = "https://steamusercontent-a.akamaihd.net/ugc/53576717526034505/A88863F87EC4E8E6A3B66EC813FC3E459700BBFD/",
-            diffuse = "https://steamusercontent-a.akamaihd.net/ugc/2325615279340409003/08D9A7248BAC7E73E539F58552FCF7B7F7B86ED5/",
-            collider = "https://steamusercontent-a.akamaihd.net/ugc/53580341274765070/1C959D9EA91D01CE2228CBE23C78E55F68F8F48F/"
+            mesh = ASSET_ROOT .. "tools/turning_tool/turning_tool_mesh.obj",
+            diffuse = ASSET_ROOT .. "tools/turning_tool/turning_tool_diffuse.png",
+            collider = ASSET_ROOT .. "tools/turning_tool/turning_tool_collider.obj"
         },
         script = [[-- empty, for now]]
     },
