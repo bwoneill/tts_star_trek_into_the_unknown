d4cb2c64ba39cd6965f2fff9d6b1c31bc087319d
Benjamin O'Neill <ben.w.oneill@gmail.com>
2025-06-04 14:59:48 -0700

    Added turning tool snap to rulers

--- code/Global.-1.lua
+++ code/Global.-1.lua
@@ -199,7 +199,33 @@ ASSETS = {
                     MeshURL = ASSET_ROOT .. "tools/turning_tool/turning_tool_mesh.obj",
                     DiffuseURL = ASSET_ROOT .. "tools/turning_tool/turning_tool_diffuse.png",
                     ColliderURL = ASSET_ROOT .. "tools/turning_tool/turning_tool_collider.obj"
-                }
+                },
+                LuaScript = [[function onDrop()
+    local rulers = getObjectsWithTag("Ruler")
+    local closest = nil
+    local dist = 12
+    local pos = self.getPosition()
+    for _, ruler in pairs(rulers) do
+        local d = (pos - ruler.getPosition()):magnitude()
+        if d < dist then
+            d = dist
+            closest = ruler
+        end
+    end
+    if closest then
+        local rot = closest.getRotation().y
+        local d = (pos - closest.getPosition()):rotateOver("y", -rot)
+        if d.z > 0 then
+            d.z = 1.3
+            self.setRotation(Vector(0, rot - 90, 0))
+        else
+            d.z = -1.3
+            self.setRotation(Vector(0, rot + 90, 0))
+        end
+        d:rotateOver("y", rot)
+        self.setPosition(d + closest.getPosition())
+    end
+end]]
             }
         }
     },
