e04f594c470d785185fd85a189c9c9ef998bf38a
Benjamin O'Neill <ben.w.oneill@gmail.com>
2025-08-29 15:46:10 -0700

    Bug fixing

--- code/ships/ship.lua
+++ code/ships/ship.lua
@@ -638,7 +638,8 @@ end
 function drawArc(system, jammed) -- system is "sensors", "comms", "weapons"
     local myShip = getObjectFromGUID(saveData.shipGUID)
     local stats = shipData[system]
-    local clr = myShip.getColorTint().setAt("a", 1)
+    local clr = myShip.getColorTint()
+    clr.setAt("a", 1)
     local geometry = BASE_CONST[shipData.size].arcs
     local lines = {}
     -- Axis overlay
